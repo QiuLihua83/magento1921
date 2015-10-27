fa3b363d0c8da0a6d4b179dc3d816eceb38b9da5
qiulihua83 <qiulihua83@163.com>
2015-10-27 18:48:35 +0800

    chmod .sh

--- tools/chmod.sh
+++ tools/chmod.sh
@@ -4,3 +4,4 @@ sudo find ${ROOT} -type f -exec chmod 644 {} \;
 sudo find ${ROOT} -type d -exec chmod 777 {} \;
 sudo chmod o+w ${ROOT}/var/.htaccess
 sudo chmod 550 ${ROOT}/mage
+sudo chmod +x  ${ROOT}/tools/chmod.sh
